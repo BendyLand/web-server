999e778c4908a4363a309fc6c784a7b6206cc5d6
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-17 11:00:13 -0400

    Create RemoveTask and helper function

--- golang/main.go
+++ golang/main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "golang/todo"
+	"fmt"
+	"golang/todo"
+	"net/http"
 )
 
 func main() {
-    var todos todo.TodoList
-    todos.CreateTask("This is my first task")
-    todos.CreateTask("This is my second task")
-    todos.DisplayTodoList()
+	var todos todo.TodoList
+	todos.CreateTask("This is my first task")
+	todos.CreateTask("This is my second task")
+	todos.CreateTask("This is my third task")
+	todos.CreateTask("This is my fourth task")
+	todos.CreateTask("This is my fifth task")
+	todos.DisplayTodoList()
+	todos.RemoveTask()
+	todos.DisplayTodoList()
 }
 
 func startServer() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, world!")
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, world!")
+	})
 
-    fmt.Println("Server is running on http://localhost:8080")
-    http.ListenAndServe(":8080", nil)
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 
 }
--- golang/todo/todo.go
+++ golang/todo/todo.go
@@ -2,10 +2,24 @@ package todo
 
 import (
 	"fmt"
+	"strconv"
 )
 
 type Task string
 type TodoList []Task
+
+func (todos *TodoList) RemoveTask() {
+	fmt.Println("Which task would you like to delete?")
+	todos.DisplayTodoList()
+	id := todos.getId()
+	var newTodoList []Task
+	for i, todo := range *todos {
+		if i != id-1 {
+			newTodoList = append(newTodoList, todo)
+		}
+	}
+	*todos = newTodoList
+}
 
 func (todos *TodoList) DisplayTodoList() {
 	fmt.Println("Todo List:")
@@ -20,3 +34,17 @@ func (todos *TodoList) CreateTask(body string) {
 	*todos = append(*todos, newTask)
 }
 
+func (todos *TodoList) getId() int {
+	var input string
+	fmt.Scanln(&input)
+	id, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("Invalid input")
+		todos.getId()
+	}
+	if id > len(*todos) {
+		fmt.Println("Invalid input. Please enter a number under:", len(*todos)+1)
+		todos.getId()
+	}
+	return id
+}
