4f6830b85057db1a6ac018ddc1d5fb42f036e17a
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-17 11:44:30 -0400

    Refactor recursive code to be iterative

--- golang/todo/todo.go
+++ golang/todo/todo.go
@@ -11,26 +11,25 @@ type Task string
 type TodoList []Task
 
 func (todos *TodoList) Loop() {
-	var input string
-	fmt.Printf("\nEnter a command or type `help`:\n")
-	fmt.Scan(&input)
-	switch input {
-	case "add":
-		todos.CreateTask()
-		todos.Loop()
-	case "delete":
-		todos.RemoveTask()
-		todos.Loop()
-	case "view":
-		todos.DisplayTodoList()
-		todos.Loop()
-	case "exit":
-		fmt.Println("Here is your final Todo List:")
-		todos.DisplayTodoList()
-		fmt.Println("Goodbye!")
-	default:
-		fmt.Printf("HELP MENU:\nThe available commands are:\nadd\ndelete\nview\nexit\nhelp\n\n")
-		todos.Loop()
+	Loop: for {
+		var input string
+		fmt.Printf("\nEnter a command or type `help`:\n")
+		fmt.Scan(&input)
+		switch input {
+		case "add":
+			todos.CreateTask()
+		case "delete":
+			todos.RemoveTask()
+		case "view":
+			todos.DisplayTodoList()
+		case "exit":
+			fmt.Println("Here is your final Todo List:")
+			todos.DisplayTodoList()
+			fmt.Println("Goodbye!")
+			break Loop
+		default:
+			fmt.Printf("HELP MENU:\nThe available commands are:\nadd\ndelete\nview\nexit\nhelp\n\n")
+		}
 	}
 }
 
@@ -44,7 +43,7 @@ func (todos *TodoList) RemoveTask() {
 	id := todos.getId()
 	var newTodoList []Task
 	for i, todo := range *todos {
-		if i != id-1 {
+		if i != id {
 			newTodoList = append(newTodoList, todo)
 		}
 	}
@@ -54,7 +53,7 @@ func (todos *TodoList) RemoveTask() {
 func (todos *TodoList) DisplayTodoList() {
 	fmt.Println("Todo List:")
 	for i, todo := range *todos {
-		fmt.Printf("%d.) %s", i+1, todo) // the todo variable ends with '\n'
+		fmt.Printf("%d.) %s", i+1, todo) // the todo variable already ends with '\n'
 	}
 	fmt.Println()
 }
@@ -62,22 +61,29 @@ func (todos *TodoList) DisplayTodoList() {
 func (todos *TodoList) CreateTask() {
 	fmt.Printf("\nPlease enter the text for your task:\n")
 	reader := bufio.NewReader(os.Stdin)
-	body, _ := reader.ReadSlice('\n')
+	body, err := reader.ReadSlice('\n')
+	if err != nil {
+		fmt.Println("There was a problem saving your task, please try again.")
+		todos.CreateTask()
+	}
 	newTask := Task(body)
 	*todos = append(*todos, newTask)
 }
 
 func (todos *TodoList) getId() int {
-	var input string
-	fmt.Scanln(&input)
-	id, err := strconv.Atoi(input)
-	if err != nil {
-		fmt.Printf("\nInvalid input. Please enter a number.\n")
-		todos.getId()
+	length := len(*todos)
+	Loop: for {
+		var input string
+		fmt.Scanln(&input)
+		id, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Printf("\nInvalid input. Please enter a number.\n")
+			continue Loop
+		}
+		if id > length {
+			fmt.Printf("\nInvalid input. Please enter a number under: %d\n", len(*todos)+1)
+			continue Loop
+		}
+		return id-1
 	}
-	if id > len(*todos) {
-		fmt.Printf("\nInvalid input. Please enter a number under: %d\n", len(*todos)+1)
-		todos.getId()
-	}
-	return id
 }
